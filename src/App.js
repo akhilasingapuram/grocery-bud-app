af9432d700fd64622ba939175a9c2c77c2ef37b1
akhilasingapuram <singapuram.akhila@gmail.com>
2023-09-29 16:08:50 +0200

    updated styles and List component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
-// import { type } from "@testing-library/user-event/dist/type";
 
 const getLocalStorage = () => {
   let list = localStorage.getItem("list");
@@ -68,7 +67,7 @@ function App() {
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
         {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-        <h3>grocery bud</h3>
+        <h3>Grocery bud</h3>
         <div className="form-control">
           <input
             type="text"
--- src/List.js
+++ src/List.js
@@ -3,32 +3,40 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 
 const List = ({ items, removeItem, editItem }) => {
   return (
-    <div className="grocery-list">
+    <table className="grocery-table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
       {items.map((item) => {
         const { id, title } = item;
         return (
-          <article className="grocery-item" key={id}>
-            <p className="title">{title}</p>
-            <div className="btn-container">
-              <button
-                type="button"
-                className="edit-btn"
-                onClick={() => editItem(id)}
-              >
-                <FaEdit />
-              </button>
-              <button
-                type="button"
-                className="delete-btn"
-                onClick={() => removeItem(id)}
-              >
-                <FaTrash />
-              </button>
-            </div>
-          </article>
+          <tr className="grocery-item" key={id}>
+            <td>{title}</td>
+            <td>
+              <div className="btn-container">
+                <button
+                  type="button"
+                  className="edit-btn"
+                  onClick={() => editItem(id)}
+                >
+                  <FaEdit />
+                </button>
+                <button
+                  type="button"
+                  className="delete-btn"
+                  onClick={() => removeItem(id)}
+                >
+                  <FaTrash />
+                </button>
+              </div>
+            </td>
+          </tr>
         );
       })}
-    </div>
+    </table>
   );
 };
 export default List;
